9f787a25bfd979eb8da34eada6f74ad3799ea0e9
Romain <romain.rublon@gmail.com>
2020-01-23 10:32:27 +0100

    List

---
+++ src/components/List.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="List">
+
+		{{this.posts}}
+		<img class="fit-picture" src="" alt="">
+		<button @click="callApi">test</button>
+	</div>
+
+</template>
+<script>
+	import axios from 'axios';
+
+	export default {
+	name: 'List',
+		props: {},
+		data() {
+		return {
+		posts: [],
+		}
+		},
+		
+		methods:{
+			callApi (){
+				axios.get(`https://api.pokemontcg.io/v1/cards?id=xy7-54`)
+				.then(response => {
+					// JSON responses are automatically parsed.
+					this.posts = response.data.cards[0].imageUrlHiRes
+				})
+				.catch(e => {
+					this.errors.push(e)
+				})
+			}	
+		}
+	}
+</script>
+
